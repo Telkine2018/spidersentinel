12d1bf475d105b59be9b64aaf947af29c698337c
Telkine2018 <Telkine2018@gmail.com>
2025-03-09 14:03:08 +0100

    Add setting to enable/disable tiny spider

--- settings.lua
+++ settings.lua
@@ -6,6 +6,14 @@ data:extend({
 		type = "bool-setting",
 		name = "spidersentinel-conservative-mining",
 		setting_type = "runtime-global",
-		default_value = true
+		default_value = true,
+		order = "spidersentinel-a"
+	},
+	{
+		type = "bool-setting",
+		name = "spidersentinel-enable-tinyspider",
+		setting_type = "startup",
+		default_value = true,
+		order = "spidersentinel-a"
 	}
 })
